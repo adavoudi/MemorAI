95b696f536a42585c2d33c4c83df675dbb124d43
Alireza Davoudi <davoudialireza@gmail.com>
2025-06-10 20:53:02 +0200

    Added data model

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,56 +1,55 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any user authenticated via an API key can "create", "read",
-"update", and "delete" any "Todo" records.
-=========================================================================*/
-const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.owner()]),
-});
+const schema = a
+  .schema({
+    Deck: a.model({
+      name: a.string().required(),
+      cards: a.hasMany("Card", "deckId"),
+      reviewFiles: a.hasMany("ReviewFile", "deckId"),
+    }),
+
+    Card: a
+      .model({
+        cardId: a.id().required(),
+        frontText: a.string().required(),
+        backText: a.string().required(),
+        srsDueDate: a.date().required(),
+        srsInterval: a.integer().required().default(1),
+        srsEaseFactor: a.float().required().default(2.5),
+        reviewInclusionDate: a.date(),
+        deckId: a.id().required(),
+        deck: a.belongsTo("Deck", "deckId"),
+      })
+      .identifier(["cardId"])
+      .secondaryIndexes((index) => [index("deckId").sortKeys(["srsDueDate"])]),
+
+    ReviewFile: a.model({
+      // They are null when the record is first created and updated upon job completion.
+      s3Path: a.string(),
+      subtitleS3Path: a.string(),
+      cardCount: a.integer().required(),
+      isListened: a.boolean().required().default(false),
+      lastListenedAt: a.datetime(),
+      cardIds: a.id().array(),
+      deckId: a.id().required(),
+      deck: a.belongsTo("Deck", "deckId"),
+    }),
+
+    Notification: a.model({
+      message: a.string().required(),
+      isRead: a.boolean().required().default(false),
+      link: a.url(),
+    }),
+  })
+  .authorization((allow) => [
+    allow.owner().to(["create", "read", "update", "delete"]),
+  ]);
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
   authorizationModes: {
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
     defaultAuthorizationMode: "userPool",
   },
 });
-
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
-
-Using JavaScript or Next.js React Server Components, Middleware, Server 
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
