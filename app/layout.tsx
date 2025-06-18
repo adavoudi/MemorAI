c4922bd019b6cbed0eb7b2a6fa1a0e816c823f0a
Alireza Davoudi <davoudialireza@gmail.com>
2025-06-18 16:53:11 +0200

    Styled the authentication page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 "use client";
 
-import { Authenticator } from "@aws-amplify/ui-react";
+import {
+  Authenticator,
+  useTheme,
+  View,
+  Image,
+  Text,
+  Heading,
+  useAuthenticator,
+  Button,
+} from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
 import type { Metadata } from "next";
@@ -8,6 +17,252 @@ import { Inter } from "next/font/google";
 import "./app.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image alt="Amplify logo" src="/Memorai.png" width={200} />
+      </View>
+    );
+  },
+
+  Footer() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Text color={tokens.colors.neutral[80]}>
+          &copy; All Rights Reserved
+        </Text>
+      </View>
+    );
+  },
+
+  SignIn: {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Sign in to your account
+        </Heading>
+      );
+    },
+    Footer() {
+      const { toForgotPassword } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <Button
+            fontWeight="normal"
+            onClick={toForgotPassword}
+            size="small"
+            variation="link"
+          >
+            Reset Password
+          </Button>
+        </View>
+      );
+    },
+  },
+
+  SignUp: {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Create a new account
+        </Heading>
+      );
+    },
+    Footer() {
+      const { toSignIn } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <Button
+            fontWeight="normal"
+            onClick={toSignIn}
+            size="small"
+            variation="link"
+          >
+            Back to Sign In
+          </Button>
+        </View>
+      );
+    },
+  },
+  ConfirmSignUp: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text></Text>;
+    },
+  },
+  SetupTotp: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text></Text>;
+    },
+  },
+  ConfirmSignIn: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text></Text>;
+    },
+  },
+  ForgotPassword: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text></Text>;
+    },
+  },
+  ConfirmResetPassword: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text></Text>;
+    },
+  },
+  SelectMfaType: {
+    Header() {
+      return <Heading level={3}>Select Desired MFA Type</Heading>;
+    },
+    Footer() {
+      return <Text></Text>;
+    },
+  },
+  SetupEmail: {
+    Header() {
+      return <Heading level={3}>Email MFA Setup</Heading>;
+    },
+    Footer() {
+      return <Text></Text>;
+    },
+  },
+};
+
+const formFields = {
+  signIn: {
+    username: {
+      placeholder: "Enter your email",
+    },
+  },
+  signUp: {
+    password: {
+      label: "Password:",
+      placeholder: "Enter your Password:",
+      isRequired: false,
+      order: 2,
+    },
+    confirm_password: {
+      label: "Confirm Password:",
+      order: 1,
+    },
+  },
+  forceNewPassword: {
+    password: {
+      placeholder: "Enter your Password:",
+    },
+  },
+  forgotPassword: {
+    username: {
+      placeholder: "Enter your email:",
+    },
+  },
+  confirmResetPassword: {
+    confirmation_code: {
+      placeholder: "Enter your Confirmation Code:",
+      label: "New Label",
+      isRequired: false,
+    },
+    confirm_password: {
+      placeholder: "Enter your Password Please:",
+    },
+  },
+  setupTotp: {
+    QR: {
+      totpIssuer: "test issuer",
+      totpUsername: "amplify_qr_test_user",
+    },
+    confirmation_code: {
+      label: "New Label",
+      placeholder: "Enter your Confirmation Code:",
+      isRequired: false,
+    },
+  },
+  confirmSignIn: {
+    confirmation_code: {
+      label: "New Label",
+      placeholder: "Enter your Confirmation Code:",
+      isRequired: false,
+    },
+  },
+  setupEmail: {
+    email: {
+      label: "New Label",
+      placeholder: "Please enter your Email:",
+    },
+  },
+};
 
 export default function RootLayout({
   children,
@@ -17,7 +272,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Authenticator>{children}</Authenticator>
+        <Authenticator formFields={formFields} components={components}>
+          {children}
+        </Authenticator>
       </body>
     </html>
   );
